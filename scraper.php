ac80f099dfdd5c14abfb28064b6ca51105e6a2f3
flowscrap <flowscrap1@gmail.com>
2015-03-31 16:16:04 +0200

    test

--- scraper.php
+++ scraper.php
@@ -1,26 +1,40 @@
 <?
-// This is a template for a PHP scraper on Morph (https://morph.io)
-// including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 
-// You don't have to do things with the ScraperWiki library. You can use whatever is installed
-// on Morph for PHP (See https://github.com/openaustralia/morph-docker-php) and all that matters
-// is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-// has at least a table called data.
+$html = scraperwiki::scrape("http://www.leboncoin.fr/_immobilier_/offres/centre/occasions/?f=p");
+
+$dom = new simple_html_dom();
+$dom->load($html);
+$annonces = $dom->find(".list-lbc a");
+
+for ($i=0; $i < 5; $i++){
+    $annonce_html = scraperwiki::scrape($annonce[$i]->getAttribute('href'));
+    $annonce_dom = new simple_html_dom();
+    $annonce_dom->load($annonce_html);
+    $titre = $annonce_dom->find("#ad_subject", 0)->plaintext;
+    $phoneLink = $annonce_dom->find("#phoneNumber a", 0);
+    if ($phoneLink->href != "") {
+        $phoneId = str_replace('javascript:getPhoneNumber("http://www2.leboncoin.fr", ', '', $phoneLink->href);
+        $phoneId = str_replace(')', '', $phoneId);
+        $jsonImg = scraperwiki::scrape("http://www2.leboncoin.fr/ajapi/get/phone?list_id=". $phoneId ."");
+        var_dump($jsonImg);
+        if ($jsonImg != '""') {
+            $i = json_decode($jsonImg);
+            $src = $i->phoneUrl;
+            if ($src != "") {
+                $md5 = md5($src);
+                if (scraperwiki::select("* from data where 'adId'=".$phoneId."")) {
+                    echo $phoneId ." already in DB!\n";
+                } else {
+                    $img = base64_encode(file_get_contents($src));
+                    scraperwiki::save_sqlite(array('adId'), array('md5' => $md5, 'adId' => $phoneId, 'titre' => $titre, 'content' => $img));
+                    echo "saved ".$phoneId." in DB\n";
+                }
+            }
+        }
+    }
+}
+
 ?>
