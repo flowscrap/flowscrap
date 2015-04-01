06a56e6c130ccae9d8840f2d89df1c8502dbfaf4
flowscrap <flowscrap1@gmail.com>
2015-04-01 06:44:14 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,13 +3,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-$html = scraperwiki::scrape("http://www.leboncoin.fr/ventes_immobilieres/787818239.htm?ca=7_s");
-//
-// // Find something on the page using css selectors
-$dom = new simple_html_dom();
-$dom->load($html);
-$phoneLink = $dom->find("#phoneNumber a", 0);
-if ($phoneLink->href != "") {
+
     $jsonImg = scraperwiki::scrape("http://www2.leboncoin.fr/ajapi/get/phone?list_id=787818239");
     var_dump($jsonImg);
     if ($jsonImg != '""') {
@@ -26,4 +20,4 @@ if ($phoneLink->href != "") {
             }
         }
     }
-}
+
