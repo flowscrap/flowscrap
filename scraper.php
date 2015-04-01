20b8a13340d45184e5682579a8c082918877d1b2
flowscrap <flowscrap1@gmail.com>
2015-04-01 07:05:21 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,7 +4,7 @@ require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
 
-                $img = base64_encode(file_get_contents(http://www.leboncoin.fr/pg/0uADF4JXB2Uz9C66TjYRtBu74ZlKopY75UDk=.gif));
+                $img = base64_encode(file_get_contents("http://www.leboncoin.fr/pg/0uADF4JXB2Uz9C66TjYRtBu74ZlKopY75UDk=.gif"));
                 scraperwiki::save_sqlite(array('adId'), array('md5' => $md5, 'adId' => "773980147", 'content' => $img));
                 echo "saved 773980147 in DB\n";
   
