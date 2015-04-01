7e36567e104e46bb0f1046c0755e94332e6d92c8
flowscrap <flowscrap1@gmail.com>
2015-04-01 06:39:22 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,43 +1,29 @@
-<?
-
+<?php
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-
-$html = scraperwiki::scrape("http://www.leboncoin.fr/_immobilier_/offres/centre/occasions/?f=p");
-
+//
+// // Read in a page
+$html = scraperwiki::scrape("http://www.leboncoin.fr/ventes_immobilieres/787818239.htm?ca=7_s");
+//
+// // Find something on the page using css selectors
 $dom = new simple_html_dom();
 $dom->load($html);
-$annonces = $dom->find(".list-lbc a");
-
-for ($i=0; $i < 5; $i++){
-    $annonce_html = scraperwiki::scrape($annonces[$i]->href);
-    $annonce_dom = new simple_html_dom();
-    $annonce_dom->load($annonce_html);
-    $titre = $annonce_dom->find("#ad_subject", 0)->plaintext;
-    $phoneLink = $annonce_dom->find("#phoneNumber a", 0);
-    if ($phoneLink->href != "") {
-
-        $phoneId = str_replace('javascript:getPhoneNumber("http://www2.leboncoin.fr", ', '', $phoneLink->href);
-        $phoneId = str_replace(')', '', $phoneId);
-        $url = "http://www2.leboncoin.fr/ajapi/get/phone?list_id=". $phoneId;
-        var_dump($url);
-        $jsonImg = file_get_contents($url);
-        var_dump($jsonImg);
-        if ($jsonImg != '""') {
-            $i = json_decode($jsonImg);
-            $src = $i->phoneUrl;
-            if ($src != "") {
-                $md5 = md5($src);
-                if (scraperwiki::select("* from data where 'adId'=".$phoneId."")) {
-                    echo $phoneId ." already in DB!\n";
-                } else {
-                    $img = base64_encode(file_get_contents($src));
-                    scraperwiki::save_sqlite(array('adId'), array('md5' => $md5, 'adId' => $phoneId, 'titre' => $titre, 'content' => $img));
-                    echo "saved ".$phoneId." in DB\n";
-                }
+$phoneLink = $dom->find("#phoneNumber a", 0);
+if ($phoneLink->href != "") {
+    $jsonImg = scraperwiki::scrape("http://www2.leboncoin.fr/ajapi/get/phone?list_id=787818239");
+    var_dump($jsonImg);
+    if ($jsonImg != '""') {
+        $i = json_decode($jsonImg);
+        $src = $i->phoneUrl;
+        if ($src != "") {
+            $md5 = md5($src);
+            if (scraperwiki::select("* from data where 'adId'='787818239'")) {
+                echo "787818239 already in DB!\n";
+            } else {
+                $img = base64_encode(file_get_contents($src));
+                scraperwiki::save_sqlite(array('adId'), array('md5' => $md5, 'adId' => "787818239", 'content' => $img));
+                echo "saved 787818239 in DB\n";
             }
         }
     }
 }
-
-?>
