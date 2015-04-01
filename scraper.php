3f6d7a1b6fd1bb2c7c37af2a4ec43969366ef8ef
flowscrap <flowscrap1@gmail.com>
2015-04-01 14:30:42 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,40 +3,24 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
-$html = scraperwiki::scrape("http://www.leboncoin.fr/_immobilier_/offres/centre/occasions/?f=p");
+$html = scraperwiki::scrape("http://www.topannonces.fr/annonces-immobilier-u7.html?type=1");
 
 $dom = new simple_html_dom();
 $dom->load($html);
-$annonces = $dom->find(".list-lbc a");
+$annonces = $dom->find("#classifiedresults .classifiedTitre a");
 
 for ($i=0; $i < 5; $i++){
     $annonce_html = scraperwiki::scrape($annonces[$i]->href);
-    $annonce_dom = new simple_html_dom();
-    $annonce_dom->load($annonce_html);
-    $titre = $annonce_dom->find("#ad_subject", 0)->plaintext;
-    $phoneLink = $annonce_dom->find("#phoneNumber a", 0);
-    if ($phoneLink->href != "") {
-
-        $phoneId = str_replace('javascript:getPhoneNumber("http://www2.leboncoin.fr", ', '', $phoneLink->href);
-        $phoneId = str_replace(')', '', $phoneId);
-        var_dump($phoneId);
-        $url = "http://www2.leboncoin.fr/ajapi/get/phone?list_id=". $phoneId;
-        $jsonImg = scraperwiki::scrape($url);
-        var_dump($jsonImg);
-        if ($jsonImg != '""') {
-            $i = json_decode($jsonImg);
-            $src = $i->phoneUrl;
-            if ($src != "") {
-                $md5 = md5($src);
-                if (scraperwiki::select("* from data where 'adId'=".$phoneId."")) {
-                    echo $phoneId ." already in DB!\n";
-                } else {
-                    $img = base64_encode(file_get_contents($src));
-                    scraperwiki::save_sqlite(array('adId'), array('md5' => $md5, 'adId' => $phoneId, 'titre' => $titre, 'content' => $img));
-                    echo "saved ".$phoneId." in DB\n";
-                }
-            }
-        }
+    $annonceDom = new simple_html_dom();
+    $annonceDom->load($annonce_html);
+    $id =  $annonceDom->find("#ClassifiedId")->value;
+    $titre = $annonceDom->find(".detailTitle h2", 0)->plaintext;
+    $phoneNb = $annonceDom->find(".zone-tel", 0)->plaintext;
+    if (scraperwiki::select("* from data where 'Id'=".$id."")) {
+        echo $phoneId ." already in DB!\n";
+    } else {
+        scraperwiki::save_sqlite(array('Id'), array('Id' => $id, 'titre' => $titre, 'Telephone' => $phoneNb));
+        echo "saved ".$id." in DB\n";
     }
 }
 
